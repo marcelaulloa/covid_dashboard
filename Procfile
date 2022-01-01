7c4f3f19c992cf66936cd2a03a537c747d6c5d60
Marcela Ulloa <marcelaperez@gmail.com>
2022-01-01 23:06:21 +0100

    streamlit deploy files

---
+++ Procfile
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red49\green49\blue49;\red83\green83\blue83;}
+{\*\expandedcolortbl;;\cssrgb\c25098\c25098\c25098;\cssrgb\c40000\c40000\c40000;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl336\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 web: sh setup.sh \cf3 \strokec3 &&\cf2 \strokec2  streamlit run COVID_dashboard_2.py\
+}
---
+++ setup.sh
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red49\green49\blue49;\red15\green112\blue1;\red169\green14\blue26;
+\red19\green8\blue105;}
+{\*\expandedcolortbl;;\cssrgb\c25098\c25098\c25098;\cssrgb\c0\c50196\c0;\cssrgb\c72941\c12941\c12941;
+\cssrgb\c9804\c9020\c48627;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl336\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+mkdir -p ~/.streamlit/\
+\cf3 echo\cf2  \cf4 "[general]\\n email = \\\'94marcelaperez@gmail.com\\"\\n"\cf2  > ~/.streamlit/credentials.toml\
+\cf3 echo\cf2  \cf4 "[server]\\n headless = true\\n enableCORS=false\\n port = \cf5 $PORT\cf4 \\n"\cf2  > ~/.streamlit/config.toml\
+}
