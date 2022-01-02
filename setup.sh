9197dc354ab29e142d6fa8e3ce38c64bcb3f2526
Marcela Ulloa <marcelaperez@gmail.com>
2022-01-02 20:11:46 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-mkdir -p ~/.streamlit/
-echo "[general]\\n email = \\"marcelaperez@gmail.com\\"\\n" > ~/.streamlit/credentials.toml
-echo "[server]\\n headless = true\\n enableCORS=false\\n port = $PORT\\n" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
