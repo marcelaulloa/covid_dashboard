5fc490b27ddc8b050ca48a9fc662b579dad6ab83
Marcela Ulloa <marcelaperez@gmail.com>
2022-01-02 19:51:55 +0100

    update procfile

--- setup.sh
+++ setup.sh
@@ -1,12 +1,2 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red49\green49\blue49;}
-{\*\expandedcolortbl;;\cssrgb\c25098\c25098\c25098;}
-\margl1440\margr1440\vieww15140\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl336\partightenfactor0
-
-\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
-mkdir -p ~/.streamlit/\
-echo "[general]\\n email = \\"marcelaperez@gmail.com\\"\\n" > ~/.streamlit/credentials.toml\
-echo "[server]\\n headless = true\\n enableCORS=false\\n port = $PORT\\n" > ~/.streamlit/config.toml}
+echo "[general]\\n email = \\"marcelaperez@gmail.com\\"\\n" > ~/.streamlit/credentials.toml
+echo "[server]\\n headless = true\\n enableCORS=false\\n port = $PORT\\n" > ~/.streamlit/config.toml
